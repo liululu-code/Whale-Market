638b9ae19b67bf8ae29f3f3981ba955e73b88923
liululu <liululu44556@163.com>
2023-05-02 16:31:59 +0800

    initation

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <array>
+#include <ctime>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::wcout;
+using std::wcin;
+using std::string;
+using std::wstring;
+using std::getline;
+
+
+
+int main()
+{
+
+    
+    
+
+    return EXIT_SUCCESS;
+}
